8f8be7192cbcc509b660617950a19c0b6e145239
Martin Bammer <mrbm74@gmail.com>
2024-06-15 09:38:25 +0200

    Update jfastlogging

--- jfastlogging/src/logger.rs
+++ jfastlogging/src/logger.rs
@@ -2,7 +2,7 @@ use jni::JNIEnv;
 
 use jni::objects::{JClass, JString};
 
-use jni::sys::{jint, jlong};
+use jni::sys::{jboolean, jint, jlong};
 
 use fastlogging::Logger;
 
@@ -17,9 +17,25 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerNew(
     domain: JString,
 ) -> jlong {
     let domain: String = env.get_string(&domain).unwrap().into();
-    let instance = Logger::new(level as u8, domain);
+    let logger = Logger::new(level as u8, domain);
+    Box::into_raw(Box::new(logger)) as jlong
+}
 
-    Box::into_raw(Box::new(instance)) as jlong
+/// # Safety
+///
+/// This function creates a new extended instance.
+#[no_mangle]
+pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerNewExt(
+    mut env: JNIEnv,
+    _class: JClass,
+    level: jint, // Global log level
+    domain: JString,
+    tname: jboolean,
+    tid: jboolean,
+) -> jlong {
+    let domain: String = env.get_string(&domain).unwrap().into();
+    let logger = Logger::new_ext(level as u8, domain, tname != 0, tid != 0);
+    Box::into_raw(Box::new(logger)) as jlong
 }
 
 /// # Safety
@@ -29,12 +45,10 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerNew(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerSetLevel(
     mut _env: JNIEnv,
     _class: JClass,
-    logger_ptr: jlong,
+    logger: &mut Logger,
     level: jint,
 ) {
-    let instance = &mut *(logger_ptr as *mut Logger);
-
-    instance.set_level(level as u8);
+    logger.set_level(level as u8);
 }
 
 /// # Safety
@@ -44,13 +58,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerSetLevel(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerSetDomain(
     mut env: JNIEnv,
     _class: JClass,
-    logger_ptr: jlong,
+    logger: &mut Logger,
     domain: JString,
 ) {
-    let instance = &mut *(logger_ptr as *mut Logger);
     let domain: String = env.get_string(&domain).unwrap().into();
-
-    instance.set_domain(domain);
+    logger.set_domain(domain);
 }
 
 /// # Safety
@@ -60,12 +72,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerSetDomain(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerTrace(
     mut env: JNIEnv,
     _class: JClass,
-    logger_ptr: jlong,
+    logger: &mut Logger,
     message: JString,
 ) {
-    let instance = &mut *(logger_ptr as *mut Logger);
     let message: String = env.get_string(&message).unwrap().into();
-    if let Err(err) = instance.trace(message) {
+    if let Err(err) = logger.trace(message) {
         env.throw(err.to_string()).unwrap();
         unreachable!();
     }
@@ -78,12 +89,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerTrace(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerDebug(
     mut env: JNIEnv,
     _class: JClass,
-    logger_ptr: jlong,
+    logger: &mut Logger,
     message: JString,
 ) {
-    let instance = &mut *(logger_ptr as *mut Logger);
     let message: String = env.get_string(&message).unwrap().into();
-    if let Err(err) = instance.debug(message) {
+    if let Err(err) = logger.debug(message) {
         env.throw(err.to_string()).unwrap();
         unreachable!();
     }
@@ -96,12 +106,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerDebug(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerInfo(
     mut env: JNIEnv,
     _class: JClass,
-    logger_ptr: jlong,
+    logger: &mut Logger,
     message: JString,
 ) {
-    let instance = &mut *(logger_ptr as *mut Logger);
     let message: String = env.get_string(&message).unwrap().into();
-    if let Err(err) = instance.info(message) {
+    if let Err(err) = logger.info(message) {
         env.throw(err.to_string()).unwrap();
         unreachable!();
     }
@@ -114,12 +123,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerInfo(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerSuccess(
     mut env: JNIEnv,
     _class: JClass,
-    logger_ptr: jlong,
+    logger: &mut Logger,
     message: JString,
 ) {
-    let instance = &mut *(logger_ptr as *mut Logger);
     let message: String = env.get_string(&message).unwrap().into();
-    if let Err(err) = instance.success(message) {
+    if let Err(err) = logger.success(message) {
         env.throw(err.to_string()).unwrap();
         unreachable!();
     }
@@ -132,12 +140,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerSuccess(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerWarning(
     mut env: JNIEnv,
     _class: JClass,
-    logger_ptr: jlong,
+    logger: &mut Logger,
     message: JString,
 ) {
-    let instance = &mut *(logger_ptr as *mut Logger);
     let message: String = env.get_string(&message).unwrap().into();
-    if let Err(err) = instance.warning(message) {
+    if let Err(err) = logger.warning(message) {
         env.throw(err.to_string()).unwrap();
         unreachable!();
     }
@@ -150,12 +157,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerWarning(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerError(
     mut env: JNIEnv,
     _class: JClass,
-    logger_ptr: jlong,
+    logger: &mut Logger,
     message: JString,
 ) {
-    let instance = &mut *(logger_ptr as *mut Logger);
     let message: String = env.get_string(&message).unwrap().into();
-    if let Err(err) = instance.error(message) {
+    if let Err(err) = logger.error(message) {
         env.throw(err.to_string()).unwrap();
         unreachable!();
     }
@@ -168,12 +174,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerError(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerCritical(
     mut env: JNIEnv,
     _class: JClass,
-    logger_ptr: jlong,
+    logger: &mut Logger,
     message: JString,
 ) {
-    let instance = &mut *(logger_ptr as *mut Logger);
     let message: String = env.get_string(&message).unwrap().into();
-    if let Err(err) = instance.critical(message) {
+    if let Err(err) = logger.critical(message) {
         env.throw(err.to_string()).unwrap();
         unreachable!();
     }
@@ -186,12 +191,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerCritical(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerFatal(
     mut env: JNIEnv,
     _class: JClass,
-    logger_ptr: jlong,
+    logger: &mut Logger,
     message: JString,
 ) {
-    let instance = &mut *(logger_ptr as *mut Logger);
     let message: String = env.get_string(&message).unwrap().into();
-    if let Err(err) = instance.fatal(message) {
+    if let Err(err) = logger.fatal(message) {
         env.throw(err.to_string()).unwrap();
         unreachable!();
     }
@@ -204,12 +208,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerFatal(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggerException(
     mut env: JNIEnv,
     _class: JClass,
-    logger_ptr: jlong,
+    logger: &mut Logger,
     message: JString,
 ) {
-    let instance = &mut *(logger_ptr as *mut Logger);
     let message: String = env.get_string(&message).unwrap().into();
-    if let Err(err) = instance.exception(message) {
+    if let Err(err) = logger.exception(message) {
         env.throw(err.to_string()).unwrap();
         unreachable!();
     }
--- jfastlogging/src/logging.rs
+++ jfastlogging/src/logging.rs
@@ -4,11 +4,11 @@ use jni::JNIEnv;
 
 use jni::objects::{JClass, JString};
 
-use jni::sys::{jboolean, jbyte, jdouble, jint, jlong};
+use jni::sys::{jboolean, jbyte, jdouble, jint, jlong, jstring};
 
 use fastlogging::{
     ClientWriterConfig, ConsoleWriterConfig, EncryptionMethod, ExtConfig, FileWriterConfig,
-    LevelSyms, Logger, Logging, MessageStructEnum, ServerConfig, WriterTypeEnum,
+    LevelSyms, Logger, Logging, MessageStructEnum, ServerConfig, WriterConfigEnum, WriterTypeEnum,
 };
 
 use crate::{get_string, throw_exception};
@@ -130,45 +130,13 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingNew(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingShutdown(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     now: jboolean,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.shutdown(now != 0) {
+    if let Err(err) = logging.shutdown(now != 0) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
-    let _boxed_logging = Box::from_raw(logging_ptr as *mut Logging);
-}
-
-/// # Safety
-///
-/// Add a Logger instance
-#[no_mangle]
-pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingAddLogger(
-    mut _env: JNIEnv,
-    _class: JClass,
-    logging_ptr: jlong,
-    logger_ptr: jlong,
-) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    let logger = &mut *(logger_ptr as *mut Logger);
-    instance.add_logger(logger);
-}
-
-/// # Safety
-///
-/// Remove a Logger instance
-#[no_mangle]
-pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingRemoveLogger(
-    mut _env: JNIEnv,
-    _class: JClass,
-    logging_ptr: jlong,
-    logger_ptr: jlong,
-) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    let logger = &mut *(logger_ptr as *mut Logger);
-    instance.remove_logger(logger);
+    let _boxed_logging = Box::from_raw(logging);
 }
 
 /// # Safety
@@ -178,17 +146,16 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingRemoveLogger(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSetLevel(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     writer: *mut WriterTypeEnum,
     level: jint,
 ) -> jlong {
-    let instance = &mut *(logging_ptr as *mut Logging);
     let writer = *Box::from_raw(writer);
-    match instance.set_level(writer, level as u8) {
+    match logging.set_level(writer, level as u8) {
         Ok(_) => 0,
         Err(err) => {
             throw_exception(&mut env, err.to_string());
-            unreachable!();
+            -1
         }
     }
 }
@@ -200,11 +167,10 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSetLevel(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSetDomain(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     domain: JString,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    instance.set_domain(get_string(&mut env, domain));
+    logging.set_domain(get_string(&mut env, domain));
 }
 
 /// # Safety
@@ -214,12 +180,79 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSetDomain(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSetLevel2Sym(
     mut _env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
-    level2sym_ptr: jlong,
+    logging: &mut Logging,
+    level2sym: &mut LevelSyms,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    let level2sym = &mut *(level2sym_ptr as *mut LevelSyms);
-    instance.set_level2sym(level2sym.to_owned());
+    logging.set_level2sym(level2sym.to_owned());
+}
+
+/// # Safety
+///
+/// Set extended configuration.
+#[no_mangle]
+pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSetExtConfig(
+    mut _env: JNIEnv,
+    _class: JClass,
+    logging: &mut Logging,
+    ext_config: &mut ExtConfig,
+) {
+    logging.set_ext_config(ext_config);
+}
+
+/// # Safety
+///
+/// Add a Logger instance
+#[no_mangle]
+pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingAddLogger(
+    mut _env: JNIEnv,
+    _class: JClass,
+    logging: &mut Logging,
+    logger: &mut Logger,
+) {
+    logging.add_logger(logger);
+}
+
+/// # Safety
+///
+/// Remove a Logger instance
+#[no_mangle]
+pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingRemoveLogger(
+    mut _env: JNIEnv,
+    _class: JClass,
+    logging: &mut Logging,
+    logger: &mut Logger,
+) {
+    logging.remove_logger(logger);
+}
+
+/// # Safety
+///
+/// Add a Writer instance
+#[no_mangle]
+pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingAddWriter(
+    mut env: JNIEnv,
+    _class: JClass,
+    logging: &mut Logging,
+    writer: &mut WriterConfigEnum,
+) {
+    if let Err(err) = logging.add_writer(writer) {
+        throw_exception(&mut env, err.to_string());
+    }
+}
+
+/// # Safety
+///
+/// Remove a Writer instance
+#[no_mangle]
+pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingRemoveWriter(
+    mut env: JNIEnv,
+    _class: JClass,
+    logging: &mut Logging,
+    writer: &mut WriterTypeEnum,
+) {
+    if let Err(err) = logging.remove_writer(writer) {
+        throw_exception(&mut env, err.to_string());
+    }
 }
 
 /// # Safety
@@ -229,15 +262,14 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSetLevel2Sym(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSync(
     mut env: JNIEnv,
     _class: JClass,
+    logging: &mut Logging,
     console: jboolean,
     file: jboolean,
     client: jboolean,
     syslog: jboolean,
-    logging_ptr: jlong,
     timeout: jdouble,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.sync(console != 0, file != 0, client != 0, syslog != 0, timeout) {
+    if let Err(err) = logging.sync(console != 0, file != 0, client != 0, syslog != 0, timeout) {
         throw_exception(&mut env, err.to_string());
     }
 }
@@ -249,13 +281,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSync(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSyncAll(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     timeout: jdouble,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.sync_all(timeout) {
+    if let Err(err) = logging.sync_all(timeout) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
 
@@ -266,18 +296,16 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSyncAll(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingRotate(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     path: JString,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
     let path = if !path.is_null() {
         Some(PathBuf::from(get_string(&mut env, path)))
     } else {
         None
     };
-    if let Err(err) = instance.rotate(path) {
+    if let Err(err) = logging.rotate(path) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
 
@@ -288,16 +316,14 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingRotate(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSetEncryption(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     writer: *mut WriterTypeEnum,
     key: *mut EncryptionMethod,
 ) {
     let writer = *Box::from_raw(writer);
     let key = *Box::from_raw(key);
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.set_encryption(writer, key) {
+    if let Err(err) = logging.set_encryption(writer, key) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
 
@@ -308,12 +334,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSetEncryption(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingGetConfig(
     _env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     writer: *mut WriterTypeEnum,
 ) -> jlong {
     let writer = *Box::from_raw(writer);
-    let instance = &mut *(logging_ptr as *mut Logging);
-    Box::into_raw(Box::new(instance.get_config(&writer))) as jlong
+    Box::into_raw(Box::new(logging.get_config(&writer))) as jlong
 }
 
 /// # Safety
@@ -323,10 +348,23 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingGetConfig(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingGetServerConfig(
     _env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
 ) -> jlong {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    Box::into_raw(Box::new(instance.get_server_config())) as jlong
+    Box::into_raw(Box::new(logging.get_server_config())) as jlong
+}
+
+/// # Safety
+///
+/// Get server configuration
+#[no_mangle]
+pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingGetServerAddress<'a>(
+    env: JNIEnv<'a>,
+    _class: JClass<'a>,
+    logging: &mut Logging,
+) -> jstring {
+    let address: String = logging.get_server_address().unwrap();
+    let address: JString = env.new_string(address).unwrap();
+    address.into_raw()
 }
 
 /// # Safety
@@ -336,10 +374,9 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingGetServerConfig(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingGetServerAuthKey(
     _env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
 ) -> jlong {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    Box::into_raw(Box::new(instance.get_server_auth_key())) as jlong
+    Box::into_raw(Box::new(logging.get_server_auth_key())) as jlong
 }
 
 /// # Safety
@@ -349,10 +386,9 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingGetServerAuthKey(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingGetConfigString(
     _env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
 ) -> jlong {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    Box::into_raw(Box::new(instance.get_config_string())) as jlong
+    Box::into_raw(Box::new(logging.get_config_string())) as jlong
 }
 
 /// # Safety
@@ -362,12 +398,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingGetConfigString(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSaveConfig(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     path: JString,
 ) -> jlong {
-    let instance = &mut *(logging_ptr as *mut Logging);
     Box::into_raw(Box::new(
-        instance.save_config(Path::new(&get_string(&mut env, path))),
+        logging.save_config(Path::new(&get_string(&mut env, path))),
     )) as jlong
 }
 
@@ -378,13 +413,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSaveConfig(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingTrace(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     message: JString,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.trace(get_string(&mut env, message)) {
+    if let Err(err) = logging.trace(get_string(&mut env, message)) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
 
@@ -395,13 +428,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingTrace(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingDebug(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     message: JString,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.debug(get_string(&mut env, message)) {
+    if let Err(err) = logging.debug(get_string(&mut env, message)) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
 
@@ -412,13 +443,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingDebug(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingInfo(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     message: JString,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.info(get_string(&mut env, message)) {
+    if let Err(err) = logging.info(get_string(&mut env, message)) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
 
@@ -429,13 +458,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingInfo(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSuccess(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     message: JString,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.success(get_string(&mut env, message)) {
+    if let Err(err) = logging.success(get_string(&mut env, message)) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
 
@@ -446,13 +473,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingSuccess(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingWarning(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     message: JString,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.warning(get_string(&mut env, message)) {
+    if let Err(err) = logging.warning(get_string(&mut env, message)) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
 
@@ -463,13 +488,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingWarning(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingError(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     message: JString,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.error(get_string(&mut env, message)) {
+    if let Err(err) = logging.error(get_string(&mut env, message)) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
 
@@ -480,13 +503,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingError(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingCritical(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     message: JString,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.critical(get_string(&mut env, message)) {
+    if let Err(err) = logging.critical(get_string(&mut env, message)) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
 
@@ -497,13 +518,11 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingCritical(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingFatal(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     message: JString,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
-    if let Err(err) = instance.fatal(get_string(&mut env, message)) {
+    if let Err(err) = logging.fatal(get_string(&mut env, message)) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
 
@@ -514,17 +533,15 @@ pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingFatal(
 pub unsafe extern "C" fn Java_org_logging_FastLogging_loggingException(
     mut env: JNIEnv,
     _class: JClass,
-    logging_ptr: jlong,
+    logging: &mut Logging,
     message: JString,
 ) {
-    let instance = &mut *(logging_ptr as *mut Logging);
     let message: String = if message.is_null() {
         "EXCEPTION".to_owned()
     } else {
         get_string(&mut env, message)
     };
-    if let Err(err) = instance.exception(message) {
+    if let Err(err) = logging.exception(message) {
         throw_exception(&mut env, err.to_string());
-        unreachable!();
     }
 }
