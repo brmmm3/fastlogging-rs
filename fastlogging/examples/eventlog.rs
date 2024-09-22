397dbf077c2f275c315130f4653785a069a26e80
Martin Bammer <mrbm74@gmail.com>
2024-09-22 15:56:03 +0200

    Update example eventlog

--- fastlogging/examples/eventlog.rs
+++ fastlogging/examples/eventlog.rs
@@ -1,17 +1,23 @@
 // Important: Must be run with admin rights!
 
-use fastlogging::{Logging, LoggingError, DEBUG};
+use fastlogging::LoggingError;
+#[cfg(windows)]
+use fastlogging::{Logging, DEBUG};
 
 fn main() -> Result<(), LoggingError> {
-    eventlog::register("fastlogging").map_err(|e| LoggingError::InvalidValue(e.to_string()))?;
-    let mut logger = Logging::new(None, None, None, None, None, None, None, Some(DEBUG), None)?;
-    logger.trace("Trace Message")?;
-    logger.debug("Debug Message")?;
-    logger.info("Info Message")?;
-    logger.success("Success Message")?;
-    logger.error("Error Message")?;
-    logger.fatal("Fatal Message")?;
-    logger.shutdown(false)?;
-    eventlog::deregister("fastlogging").map_err(|e| LoggingError::InvalidValue(e.to_string()))?;
+    #[cfg(windows)]
+    {
+        eventlog::register("fastlogging").map_err(|e| LoggingError::InvalidValue(e.to_string()))?;
+        let mut logger = Logging::new(None, None, None, None, None, None, None, Some(DEBUG), None)?;
+        logger.trace("Trace Message")?;
+        logger.debug("Debug Message")?;
+        logger.info("Info Message")?;
+        logger.success("Success Message")?;
+        logger.error("Error Message")?;
+        logger.fatal("Fatal Message")?;
+        logger.shutdown(false)?;
+        eventlog::deregister("fastlogging")
+            .map_err(|e| LoggingError::InvalidValue(e.to_string()))?;
+    }
     Ok(())
 }
