0f5509fad11e787da22a8ad3ec5c38604f1268bb
Marty B. <mrbm74@gmail.com>
2026-09-06 11:28:03 +0200

    Step 4: Update Eclipse IDE Configuration - No verification needed
    - Updated .classpath: JavaSE-1.8 -> JavaSE-25 (both projects)
    - Updated .settings/org.eclipse.jdt.core.prefs: targetPlatform, compliance, source 1.8->25 (both projects)

--- jfastlogging-ffm/FastLogging/src/main/java/org/logging/FastLogging.java
+++ jfastlogging-ffm/FastLogging/src/main/java/org/logging/FastLogging.java
@@ -3,7 +3,6 @@ package org.logging;
 import java.lang.foreign.Arena;
 import java.lang.foreign.Linker;
 import java.lang.foreign.MemorySegment;
-import java.lang.foreign.SegmentAllocator;
 import java.lang.foreign.SymbolLookup;
 
 public class FastLogging {
