09fedfbca1e8d5f345cfb4a3716229ab20206d1f
Marty B. <mrbm74@gmail.com>
2026-09-14 17:01:51 +0200

    Fix build issue in pyfastlogging on github

--- pyfastlogging/src/lib.rs
+++ pyfastlogging/src/lib.rs
@@ -21,7 +21,6 @@ fn shutdown_at_exit() -> Result<(), LoggingError> {
 
 /// Python API
 #[pymodule]
-#[pyo3(name = "pyfastlogging")]
 fn init(py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     m.add("EXCEPTION", fastlogging::EXCEPTION)?;
